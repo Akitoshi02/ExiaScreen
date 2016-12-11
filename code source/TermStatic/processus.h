f1efe779a27cecb24debf947bed916a9166d5d17
Akitoshi <mandel.vaubourg@viacesi.fr>
2016-12-11 18:18:26 +0100

    ajout code source moteur rendu et final Static

---
+++ code source/TermStatic/processus.h
@@ -0,0 +1,7 @@
+#ifndef FPROCESSUS
+#define FPROCESSUS
+#include "pbm.h"
+
+pid_t create_process(void);//Permet de créer un processus
+
+#endif
---
+++ code source/TermStatic/virtuel.c
@@ -0,0 +1,201 @@
+#include "virtuel.h"
+
+
+void Createtabvir(TABVIRTUEL *tabvir, int H, int L)
+{
+    int i = 0;
+
+    //Notre tableau aura H * ligne
+
+    tabvir->H = H;
+    tabvir->L = L;
+
+    tabvir->data = malloc(H * sizeof(char *));
+    for(i = 0; i < H; i++)
+    {
+        tabvir->data[i] = (char *)malloc(L * sizeof(char *));
+    }
+}
+
+
+
+//Fonction d'affichage
+void afftabvir(TABVIRTUEL * tabvir)
+{
+   system("clear");
+    struct winsize sterm;
+    ioctl(0, TIOCGWINSZ, &sterm);
+    int i = 0, y = 0, z = 0;
+
+    //On mets au milieu de la hauteur
+    for(i = 0; i < ((sterm.ws_row / 2) - (tabvir->H / 2)); i++)
+    {
+        printf("\n");       
+    }
+
+    //On écrit le tableau 
+
+    
+    for(i = 0; i < tabvir->H; i++)
+    {
+        //On mets au millieu de la colonne
+        for(z = 0; z < ((sterm.ws_col / 2) - (tabvir->L / 2)); z++)
+        {
+            printf(" ");
+        }
+        for(y = 0; y < tabvir->L; y++)
+        {
+            if(tabvir->data[i][y] == '1')
+            {
+                printf("█");
+                //█
+            }
+            if(tabvir->data[i][y] == '0')
+            {
+                printf(" ");
+            }
+            if(tabvir->data[i][y] == '+')
+            {
+                printf("+");
+            }
+            
+        }
+        printf("\n");
+    }
+    for(i = 0; i < ((sterm.ws_row / 2) - (tabvir->H / 2)); i++)
+    {
+        printf("\n");       
+    }
+}
+
+//Fonction permettant de gérer l'affichage sur un thread différent
+void *affthread(TABVIRTUEL * tabvir)
+{  
+    while(1)
+    {
+        afftabvir(tabvir);
+        sleep(1);
+    }
+    pthread_exit(0);    
+}
+
+void moteurrendu(TABVIRTUEL *tabvir, PBM *file)
+{
+
+    //variable de boucle
+    int i = 0, y = 0, z = 0, g = 0, h = 0;
+    //variable curseur
+    int curseurH = 0, curseurL = 0;
+    //variable pixel
+    int pixelH = 0, pixelL = 0;
+    PIXEL pixel;
+    pixel.H = 0;
+    pixel.L = 0;
+    //variable lecture caractère
+    char pixelnow = 0;
+
+
+      //module de rendu
+
+      //On nettoie le tableau
+    for(i = 0; i < tabvir->H; i++)
+    {
+        for(y = 0; y < tabvir->L; y++)
+        {
+            tabvir->data[i][y] = 0;
+        }
+    }
+    
+    //Permet de calculer la taille d'un pixel
+    if(file->L < tabvir->L)
+    {
+
+        pixelL = tabvir->L / file->L;       
+    }
+    else
+    {
+        pixelL = 1;  
+    }
+    if (file->H < tabvir->H)
+    {
+         pixelH = tabvir->H / file->H;
+    }
+    else
+    {      
+        pixelH = 1;
+    }
+
+
+    //On rempli le tableau en redimenssionant les pixel du fichier pour le remplir
+    //On centre le .pbm au centre du tableau virtuel
+
+     curseurH = ((tabvir->H - (file->H * pixelH))) / 2;
+    if(curseurH < 1)
+    {
+        curseurH = 0;
+    }
+    curseurL = ((tabvir->L - (file->L * pixelL))) / 2;
+    if(curseurL < 1)
+    {
+        curseurL = 0;
+    }
+    
+  
+    //Puis on place dans le tableau le .pbm
+    //On prend en compte deux possibilité si la taille de limage est inférieur ou égale à la taille du tableau virtuel
+    //Ou si la taille est supérieur
+    if(file->H <= tabvir->H &&  file->L <= tabvir->L )
+    {
+
+
+        for(i = 0; i < file->H; i++)
+        {
+
+            for(y = 0; y < file->L; y++)
+            {  
+                pixelnow = file->data[i][y];
+
+                for(z = 0; z < pixelH; z++)
+                {
+                
+                    for(g = 0; g < pixelL; g++)
+                    { 
+                        tabvir->data[z + curseurH][g + curseurL] = pixelnow;                 
+                    }               
+                }
+           
+                curseurL = curseurL + pixelL;
+            }
+            curseurL = ((tabvir->L - (file->L * pixelL))) / 2;
+            curseurH = curseurH + pixelH;
+        }
+        curseurH = 0;
+
+    }
+    else//Si la taille est supérieur 
+    {
+       for(i = 0; i < tabvir->H; i++)
+        {
+
+            for(y = 0; y < tabvir->L; y++)
+            {  
+                 pixelnow = file->data[i][y];
+
+                for(z = 0; z < pixelH; z++)
+                {
+                
+                    for(g = 0; g < pixelL; g++)
+                    { 
+                        tabvir->data[z + curseurH][g + curseurL] = pixelnow;     
+                              
+                    }               
+                }
+                curseurL = curseurL + pixelL;
+            }
+            curseurL = ((tabvir->L - (file->L * pixelL))) / 2;
+            curseurH = curseurH + pixelH;
+        }
+        curseurH = 0;
+     
+    }
+}
